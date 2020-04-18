5bcc068065223df479dbb1b3b665018333b63e2c
Mehedi Islam Ripon <51530291+MehedilslamRipon@users.noreply.github.com>
2020-04-18 23:35:03 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// get and set
+// var dhorbo = $(".table").text();
+// $(".fan").html("<h1>" + dhorbo + "</h1>");
+
+// adding css property chaining system
+// $(".table").css({'color' : 'yellow', 'background' : 'red'});
+
+// adding css class another way
+// $(".table").addClass("mehedi").removeClass("table");
+
+// annonymous content hide and show
